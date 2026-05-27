3891698b7fa9a66593c29b4e6b98238447b71c46
pavanorappu <pavan08.work@gmail.com>
2026-05-27 04:26:39 +0000

    updated app file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World from Docker + Jenkins + GitHub! and updated"
+    return "Hello Macha-Project Jenkins is working / sikro va da vera yadhuna project panla"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
